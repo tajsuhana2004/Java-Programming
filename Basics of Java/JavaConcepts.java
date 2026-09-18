bfda51e11552f2ef91ea7d6323d69461c199d655
Suhana Taj <tajsuhana2004@gmail.com>
2026-09-18 19:50:41 +0530

    Add JavaConcepts class with main method

---
+++ Basics of Java/JavaConcepts.java
@@ -0,0 +1,6 @@
+public class JavaConcepts {
+    public static void main(String[] args) {
+        System.out.println("**************************************");
+        System.out.println("Welcome To My Java Programming Journey");
+    }
+}
